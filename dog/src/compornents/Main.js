bae89b7ab3a0e40d47a56a9e145877a30fbd022d
dc_h_otsuka <haruki.otsuka@g.dreamcareer.co.jp>
2023-05-27 11:52:09 +0900

    add Form

--- dog/src/compornents/Main.js
+++ dog/src/compornents/Main.js
@@ -1,18 +1,29 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/main.css";
 import { fetchImages } from "./api";
 import Gallery from "./Gallery";
+import Form from "./Form";
 
 function Main() {
-    const [urls, setUrls] = useState(null);
-    useEffect(() => {
-        fetchImages("shiba").then((fetchedUrls) => {
-            console.log(fetchedUrls);
-            setUrls(fetchedUrls);
-        });
-    }, []);
+  const [urls, setUrls] = useState(null);
+  useEffect(() => {
+    fetchImages("shiba").then((fetchedUrls) => {
+      console.log(fetchedUrls);
+      setUrls(fetchedUrls);
+    });
+  }, []);
+  function reloadImages(breed) {
+    fetchImages(breed).then((urls) => {
+        setUrls(urls);
+    });
+  }
   return (
     <>
+      <section className="section">
+        <div className="container">
+          <Form onFormSubmit={reloadImages} />
+        </div>
+      </section>
       <section className="section">
         <div className="container">
           <Gallery urls={urls} />
